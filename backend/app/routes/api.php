3bf4db864f9dea8c092fc7cf4bf77bfa09bd68e6
Rafael-pin <rafa.pinheiro.pinheiro@gmail.com>
2022-02-16 15:44:28 -0300

    fix controller route

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -14,5 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/new_user', 'UserController@create')->name('users.create');
-});
+Route::post('/new_user', 'App\Http\Controllers\UserController@create')->name('users.create');
