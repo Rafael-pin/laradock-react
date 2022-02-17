57ac62eb29ab2123982d2fb6ed7f16950f31a48e
Rafael-pin <rafa.pinheiro.pinheiro@gmail.com>
2022-02-16 21:49:51 -0300

    clear userController

--- backend/app/app/Http/Controllers/UserController.php
+++ backend/app/app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\User\StoreUser;
 use App\Services\ResponseService;
 use App\Transformers\User\UserResource;
 use App\Transformers\User\UserResourceCollection;
+
 class UserController extends Controller
 {
 
@@ -32,57 +33,5 @@ class UserController extends Controller
 
         return new UserResource($user, array('type' => 'store','route' => 'user.store'));
     
-    }
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        return response()->json([
-            'product'=>$user
-        ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        $request->validate([
-            'name'=>'required',
-            'email'=>'required',
-            'company_id'=>'required'
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
     }
 }
